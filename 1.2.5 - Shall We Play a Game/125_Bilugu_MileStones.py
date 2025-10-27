d24e6c7cbf93c42c9ed07faff4ac5f56ab1935a7
Sidharth Bilugu <312634@shakopeeschools.org>
2025-10-27 09:42:59 -0500

    Milestone commits
    Developed a basic list of milestones to work toward. List may change if deemed appropriate.

---
+++ 1.2.5 - Shall We Play a Game/125_Bilugu_MileStones.py
@@ -0,0 +1,4 @@
+#TODO Day 1 - Develop main game screen, establish enemy function, add Base HP,  add game timer, add score counter, establish base player damage
+#TODO Day 2 - Create different enemy types,  develop lanes of enemy pursuit to base,  give enemies strictly linear movement, add enemy point systems
+#TODO Day 3 - Make a start screen, make a difficulty selection screen, make  different game settings for each difficulty,  make a length selection screen, make different game timers for different game lengths,  add bonus points depending on game difficulty and length
+#TODO Day 4 - Add power-ups, add power-up spawn frequencies
