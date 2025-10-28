0097765929cafcd3b0d5d9a91530a20969811237
Sidharth Bilugu <312634@shakopeeschools.org>
2025-10-28 09:01:02 -0500

    Game Variables
    Added essential game variables

--- 1.2.5 - Shall We Play a Game/125_Bilugu_MileStones.py
+++ 1.2.5 - Shall We Play a Game/125_Bilugu_MileStones.py
@@ -2,3 +2,18 @@
 #Imports
 import turtle as trtl
 import random as rand
+
+#Game Variables
+Base_HP = 100
+Score = 0
+Game_Duration = 30
+Remaining_Time = Game_Duration
+Enemies = []
+All_Lanes = [-225, -150, -75, 0, 75, 150, 225]
+Lanes = All_Lanes[:]
+game_running = False
+spawn_interval = 1000
+available_enemy_types = ["E1", "E2", "E3", "Boss"]
+difficulty_bonus = 0
+length_bonus = 0
+Base_Damage = 5
